2cfb729785e36a9566e3356b4398ec58111d758a
superpotato9 <50345854+superpotato9@users.noreply.github.com>
2022-01-24 19:29:13 -0800

    Update code.py
    fixed tone systen

--- code.py
+++ code.py
@@ -1,13 +1,16 @@
-# Write your code here :-)
+
+         # Pause between notes
+         
 print('welcome to keeb synth serial')
-print('version .5')
+print('version 1.0')
 print('copright nathan koliha 2022 released under the Mit license')
 import re
 import keypad
 import time
 import board
 import digitalio
-import simpleio
+import pwmio
+piezo = pwmio.PWMOut(board.GP16, duty_cycle=0, frequency=440, variable_frequency=True)
 real_keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#']
 km = keypad.KeyMatrix(
     row_pins=(board.GP2, board.GP7, board.GP6, board.GP4),
@@ -41,9 +44,16 @@ while True:
             #print(event)
             time.sleep(.2)
             event = km.events.get()
-	    led.value = True
+            led.value = True
+           
+
 
             pressed = real_keys[int(str(last).replace('<Event: key_number', '').replace('pressed>',''))]
-            simpleio.tone(board.GP16, TONE_FREQ[pressed], d)
             print('key', pressed)
-        
+            f = TONE_FREQ[pressed]
+            piezo.frequency = f
+            piezo.duty_cycle = 65535 // 2  # On 50%
+            time.sleep(0.05)  # On for 1/4 second
+        piezo.duty_cycle = 0# Off
+            
+            
